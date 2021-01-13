1414d3da7811966ce21043d0c4d2578ca390fbdf
Panda-0129 <boj01291127@gmail.com>
2021-01-13 08:31:32 +0800

    update readme

--- write_to_table_md.py
+++ write_to_table_md.py
@@ -1,3 +1,5 @@
+import re
+
 from lc_retriever import *
 
 COOKIE = '_ga=GA1.2.1754550287.1598943501; __stripe_mid=e3f7598a-9434-4252-a7ab-b7' \
@@ -20,19 +22,56 @@ COOKIE = '_ga=GA1.2.1754550287.1598943501; __stripe_mid=e3f7598a-9434-4252-a7ab-
          '7e66d261fd0c70ed50-1610382681-1800-Aa3JvPkYpaNxnG5nWU4snM5c1VfexDAlUE+er' \
          'hzWnaEoHzzA0gHN0Z2LUcij4r6al2HYxVllsRH+ro7SvYs7n4Y=; _gat=1'
 
+
+def embedded_numbers(s):
+    pieces = re_digits.split(s)
+    pieces[1::2] = map(int, pieces[1::2])
+    return pieces
+
+
 if __name__ == '__main__':
-    d = LCDataRetriever(COOKIE)
-    # for page in range()
-    all_sub = d.retrieve_submissions(verbose=True)
-    all_accepted_sub = []
-    for sub in all_sub:
-        if sub['status_display'] == 'Accepted' and sub['lang'] == 'python3':
-            all_accepted_sub.append(sub)
+
+    all_md_file = []
+    all_accepted_title = []
+
+    path = './Markdown'
+    files = os.listdir(path)
+    re_digits = re.compile(r'(\d+)')
+    files = sorted(files, key=embedded_numbers)
+    for file in files:
+        all_md_file.append(file)
+        file = file.replace(".md", "")
+        tmp = file.split()[1:]
+        file = " ".join(tmp)
+        all_accepted_title.append(file)
 
     md = '| #    | Title | Difficulty | runtime | memory | \n | ---- | ----- | ---------- | ------- | ------ | \n '
-    for sub in all_accepted_sub[0:10]:
-        question_info = d.retrieve_question(d.naive_title_slug(sub['title']))
-        question_id = question_info['questionId']
-        Title =
-        cur_status = f'|{question_id}||||'
+
+    d = LCDataRetriever(COOKIE)
+    all_sub = d.retrieve_submissions(verbose=True)
+    sub_dict = {}
+
+    for sub in all_sub:
+        if sub['status_display'] == 'Accepted':
+            sub_dict[d.naive_title_slug(sub['title'])] = sub
+
+    for title, md_title in zip(all_accepted_title, all_md_file):
+        try:
+            d_ = LCDataRetriever(COOKIE)
+            naive_title = d_.naive_title_slug(title)
+            print(naive_title)
+            question_info = d_.retrieve_question(naive_title)
+            if question_info is not None:
+                question_id = question_info['questionId']
+                Title = f'[{naive_title}](https://github.com/Panda-0129/leetcode/blob/master/Markdown/{md_title})'
+                difficulty = question_info['difficulty']
+                runtime = sub_dict[naive_title]['runtime']
+                memory = sub_dict[naive_title]['memory']
+                cur_status = f'|{question_id}|{Title}|{difficulty}|{runtime}|{memory}| \n'
+                md += cur_status
+        except:
+            continue
+
+    with open('README.md', 'w', encoding='utf-8') as f:
+        f.write(md)
     pass
